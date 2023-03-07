189d6dd6eb75f29106e3e33542e9294cffdf48d2
Grimlli <adam.hedberg.ahj@gmail.com>
2023-03-07 17:25:35 +0100

    Made some comments and took away old code

--- src/test/java/com/example/produktapi/Selenium.java
+++ src/test/java/com/example/produktapi/Selenium.java
@@ -36,7 +36,6 @@ public class Selenium {
 
         driver.get("https://java22.netlify.app/");
 
-
         // Temp: Slows the code down so the site can load. Did not figure out how to do it with list.
         WebElement waiter = new WebDriverWait(driver,
                 Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated((By.className("productItem"))));
@@ -49,27 +48,17 @@ public class Selenium {
     }
 
     @Test
-    public void priceForThreeProducts(){
+    public void assertThePriceForTheProductIsRight(){
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://java22.netlify.app/");
-
-        // //*[@id="productsContainer"]/div/div[1]/div/div/p/text()[1]
-        // //*[@id="productsContainer"]/div/div[1]/div/div/p/text()[2]
-
-
         String[] expected = {"109.95","22.3","55.99","15.99","695","168"};
         for(int i = 1; i<6; i++){
             WebElement waiter = new WebDriverWait(driver,
                     Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated((By.xpath(
                             "//*[@id=\"productsContainer\"]/div/div["+i+"]/div/div/p"))));
-            //WebElement pText = driver.findElement(By.xpath("//*[@id=\"productsContainer\"]/div/div[1]/div/div/p/text()[2]"));
-            //WebElement h1Text = driver.findElement(By.className("card-text"));
-            //List<WebElement> product = driver.findElements(By.className("card-text"));
-            System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-            System.out.println(waiter.getText());
             String[] result = waiter.getText().split("\\R");
-            System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
             assertEquals(expected[i-1],result[1]);
         }
 
